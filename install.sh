6c1c7eb8ac5ac5f1e43eaadcffd0db978db7fc00
Aravind Naik <132479515+Arvind9618@users.noreply.github.com>
2024-06-22 23:59:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,12 @@ sudo apt update
 sudo apt upgrade -y
 
 # Install necessary packages
-sudo apt install -y curl apt-transport-https ca-certificates software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl GnuPG
+sudo mkdir -p -m 755 /etc/apt/keyrings
 
 # Add Kubernetes repository
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo add-apt-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+curl -fsSL [2](https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key) | sudo gpg -- dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring. gpg 
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring. gpg] [3](https://pkgs.k8s). io/core: /stable : /v1. 28/deb/ /' | sudo tee /etc/apt/sources. list.d/kubernetes. list
 
 # Disable swap
 sudo swapoff -a
